4d4938029bded81855ab3a1e3276d6863ed17761
Don Koteyka <don.kotejka@yandex.ru>
2023-06-04 13:48:49 +0300

    fixes #1

--- HW_2.1.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists employee (
-id SERIAL PRIMARY KEY,
-name varchar(20) not null,
-chief varchar(20) unique,
-department integer unique
-);
--- HW_2.sql
+++ HW_2.sql
@@ -1,40 +1,45 @@
+create table if not exists album (
+id SERIAL PRIMARY key,
+name varchar(20) UNIQUE not null,
+year integer,
+-- for year of realise use year not older then 1984
+CONSTRAINT check_age_1 CHECK (year > '1984')
+);
 create table if not exists soundtrack (
 id SERIAL PRIMARY KEY,
 name varchar(20) not null,
 time integer,
-album_id integer unique
+-- for time use seconds but no more then 600 seconds
+album_id integer UNIQUE references album(id),
+CONSTRAINT check_time_1 CHECK (time < '600')
 );
-
-create table if not exists album (
-id SERIAL PRIMARY key references soundtrack(id),
-name varchar(20) not null,
-year integer,
-singer_id integer unique
-);
-
 create table if not exists singer(
 id serial primary key,
-nickname varchar(20) not null
+nickname varchar(20) UNIQUE not null
 );
 create table if not exists singeralbum(
 singerid integer references singer(id),
 albumid integer references album(id),
-constraint pk primary key (singerid, albumid)
+constraint pk_1 primary key (singerid, albumid)
 );
 create table if not exists genre(
 id serial primary key,
-type varchar(20)
+type varchar(20) unique not null
 );
 create table if not exists singergenre(
-id serial primary key,
 singerid integer references singer(id),
-genreid integer references genre(id)
+genreid integer references genre(id),
+constraint pk_2 primary key (singerid, genreid)
 );
 create  table if not exists collection(
 id serial primary key,
-name varchar(20) not null,
-year date not null,
-trackid INTEGER NOT NULL REFERENCES soundtrack(id)
+name varchar(20) UNIQUE not null,
+year integer not null,
+-- for year of realise use year not older then 1984
+CONSTRAINT check_age_2 CHECK (year > '1984')
 );
-
-
+create table if not exists soundtrackcollection(
+soundtrackid integer references soundtrack(id),
+collectionid integer references collection(id),
+constraint pk_3 primary key (soundtrackid, collectionid)
+);
